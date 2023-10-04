90423d4e047fe7933b97a0b916ec0891551ccd81
Samarth Jain <samarthjain28.2002@gmail.com>
2023-10-04 13:00:56 +0530

    initial

---
+++ lab2/VariableDemo.java
@@ -0,0 +1,17 @@
+public class VariableDemo{
+    public static void main(String [] args){
+        //primitive data type
+        int age = 25 ;
+        double pi = 3.14;
+        char ini = 'J' ;
+        boolean isStudent = true ;
+
+        //Reference Data Type
+        String name = "Samarth Jain" ;
+        int []arr = {1,2,3,4,5};
+
+        //output
+        System.out.println("Age: "+age);
+        System.out.println("Numbers: "+java.util.Arrays.toString(arr));
+    }
+}
